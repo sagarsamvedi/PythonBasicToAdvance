5087c0e47632f066536df8a26c6f9ca9983809ad
sagar samvedi <sagarsamvedi31@gmail.com>
2023-12-20 16:12:14 +0530

    added elevator code

---
+++ elevator.py
@@ -0,0 +1,55 @@
+import time
+
+current_floor = 0
+
+def elevate(user_floor):
+    global current_floor
+    while user_floor != current_floor: 
+        current_floor += 1
+        time.sleep(1)
+        print(f'You are on floor no. {current_floor}')
+    time.sleep(1)
+    print('You are on the desired floor\n')
+
+def depression(user_floor):
+    global current_floor
+    while user_floor != current_floor: 
+        print(f'You are on floor no. {current_floor}')
+        current_floor -= 1
+        time.sleep(1)        
+    time.sleep(1)
+    print('You are on the desired floor\n')
+
+def destination():
+    while True:
+        floor = int(input('Enter your desired floor between -2 to 10: '))
+        if floor in range(-2, 11):
+            break
+        else:
+            print('Enter a valid floor')
+
+    return floor
+
+def operate(user_floor):
+    global current_floor
+    if user_floor == current_floor:
+        print('You are on the desired floor')
+    elif user_floor > current_floor:
+        elevate(user_floor)
+    else:
+        depression(user_floor)
+
+user_floor = destination()
+operate(user_floor)
+
+while True:
+    print('Do you want to continue or exit?')
+    choice = input('Press E for exit or C for continue: ')
+    if choice.upper() == 'E':
+        print('Thank you for using our service')
+        break
+    elif choice.upper() == 'C':
+        user_floor = destination()
+        operate(user_floor)
+    else:
+        print('Enter a valid choice')
---
+++ login.py
@@ -0,0 +1,37 @@
+def signUp():
+    username = input('Enter new username:   ')
+    
+    while True:
+        password = input('Enter a new password: ')
+        
+        if len(password) < 8:
+            print('Password must be of length greater than or equal to 8')
+            continue
+        else:
+            has_upper = any(c.isupper() for c in password)
+            has_lower = any(c.islower() for c in password)
+            has_digit = any(c.isdigit() for c in password)
+            has_special = any(c in ['@', '#', '&', '$'] for c in password)
+
+            if has_upper and has_lower and has_digit and has_special:
+                break
+            else:
+                print('Your password must satisfy the criteria:')
+                if not has_upper:
+                    print('- At least one uppercase alphabet (A - Z)')
+                if not has_lower:
+                    print('- At least one lowercase alphabet (a - z)')
+                if not has_digit:
+                    print('- At least one digit between 0 - 9')
+                if not has_special:
+                    print('- At least one of the special characters (@, #, &, $)')
+
+    print('Account successfully created!')
+
+signUp()
+
+
+def logIn(username, password):
+    pass
+
+signUp()
